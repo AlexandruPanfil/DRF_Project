9e85873df788a32453ff02a0246bdfff8b5e3d76
AlexandruPanfil <111560446+AlexandruPanfil@users.noreply.github.com>
2024-05-10 22:37:23 +0300

    used basic API in new class, also did a get/post function (without serializer) for that instance

--- PC/views.py
+++ PC/views.py
@@ -1,6 +1,9 @@
+from django.forms import model_to_dict
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import PC
 from .serializers import PCSerializer
@@ -13,6 +16,25 @@ def index(request):
     return HttpResponse('Hello')
 
 
-class PCAPIView(generics.ListAPIView):
-    queryset = PC.objects.all()
-    serializer_class = PCSerializer
+class PCAPIView(APIView):
+    def get(self, request):
+        list_of_objects = PC.objects.all().values()
+        return Response({'title': 'All Objects', 'objects': list_of_objects,})
+
+    def post(self, request):
+        try:
+            post_new = PC.objects.create(
+                title = request.data['title'],
+                content = request.data['content'],
+                cat_id = request.data['cat_id'],
+                )
+            status_code = 201
+            return Response({'post': model_to_dict(post_new), 'success': status_code, })
+        except Exception as e:
+            return ({'Exception': e})
+
+
+
+# class PCAPIView(generics.ListAPIView):
+#     queryset = PC.objects.all()
+#     serializer_class = PCSerializer
